4e434e6a44b0d5d1c76b850eb353dd91b4c40b66
Nils Mittler <nils.mittler@computational.bio.uni-giessen.de>
2023-03-30 14:56:10 +0200

    improve control flow

--- src/auth/authService.ts
+++ src/auth/authService.ts
@@ -28,19 +28,30 @@ authService.use('/local', LocalStrategy);
  * Refresh JWT
  */
 authService.post('/refresh', function (req, res, next) {
+  // prepare auth header and cookie parsing for refresh token check
+  const authHeader = req.headers['authorization'];
+  const cookies = req.headers.cookie?.split(";")?.reduce((obj, c) => {
+    var n = c.split("=");
+    obj[n[0].trim()] = n[1].trim();
+    return obj
+  }, {});
+
+  if (!authHeader || !cookies || !cookies['token']) {
+    return res.status(400).json('Invalid request.');
+  }
+
   // get and check refresh token
-  const refreshToken = req.headers['token'] as string;
+  const refreshToken = cookies['token'];
   const refreshTokenSecret = getValidSignatureKey(refreshToken);
   if (!refreshTokenSecret) {
-    return res.status(401).json('Invalid refresh token signature.');
+    return res.status(401).json('Invalid refresh token.');
   }
 
   // get and check token
-  const bearerHeader = req.headers['authorization'] as string;
-  const token = bearerHeader.split(' ')[1];
+  const token = authHeader.split(' ')[1];
   const tokenSecret = getValidSignatureKey(token);
   if (!tokenSecret) {
-    return res.status(401).json('Invalid token signature.');
+    return res.status(401).json('Invalid access token.');
   }
 
   // verify token with refreshtoken
@@ -49,22 +60,13 @@ authService.post('/refresh', function (req, res, next) {
     const refreshTokenPayload = jwt.verify(refreshToken, refreshTokenSecret);
 
     if (refreshTokenPayload['id'] == tokenPayload['user']['id']) {
-      const user: User = tokenPayload['user'] as User;
-      respondTokens(user, res);
+      return respondTokens(tokenPayload['user'] as User, res);
+    } else {
+      return res.status(401).json('Invalid token.');
     }
   } catch {
     return res.status(401).json('Invalid token.');
   }
-});
-
-/* POST /logout
- *
- * This route logs the user out.
- */
-authService.post('/logout', function (req, res, next) {
-  req.logout(() => {
-    res.redirect('/');
-  });
 });
 
 export default authService;
