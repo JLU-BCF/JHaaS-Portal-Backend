cfa9c7894ce4185f48fc35744b08cc6ccdd31a9e
Nils Mittler <nils.mittler@computational.bio.uni-giessen.de>
2023-07-18 16:42:04 +0200

    update env files

--- src/config/Config.ts
+++ src/config/Config.ts
@@ -7,7 +7,6 @@ import Participation from '../models/Participation';
 
 export const NODE_ENV: string = process.env.NODE_ENV || '';
 export const APP_PORT: number = Number(process.env.APP_PORT) || 8000;
-export const APP_PATH: string = process.env.APP_PATH || '/api';
 export const JH_DOMAIN: string = process.env.JH_DOMAIN || 'jhaas.local';
 export const RELEASE_NAME: string = process.env.RELEASE_NAME || 'jhaas-portal';
 export const FRONTEND_URL: string = process.env.FRONTEND_URL || '/';
