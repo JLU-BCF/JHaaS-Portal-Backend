f3dd66d0310e13adcf1b679b766c40e5ce5bef05
Nils Mittler <nils.mittler@computational.bio.uni-giessen.de>
2023-09-29 13:37:02 +0200

    include frontend configuration

--- src/config/Frontend.ts
+++ src/config/Frontend.ts
@@ -2,5 +2,6 @@ export default {
   AUTHENTIK_FQDN: process.env.AUTHENTIK_FQDN || 'authentik',
   AUTHENTIK_URL: process.env.AUTHENTIK_URL || 'http://authentik:9000',
   AUTHENTIK_NAME: process.env.AUTHENTIK_NAME || 'Authentik',
-  MAIL_FEEDBACK_ADDRESS: process.env.MAIL_FEEDBACK_ADDRESS || 'feedback@jhaas.local'
+  MAIL_FEEDBACK_ADDRESS: process.env.MAIL_FEEDBACK_ADDRESS || 'feedback@jhaas.local',
+  TOS_ADDRESS: process.env.TOS_ADDRESS || '/api/tos/latest'
 };
