584730af072d8a7dcfa7e9232d9376b8fc38cd88
Nils Mittler <nils.mittler@computational.bio.uni-giessen.de>
2023-10-06 14:13:27 +0200

    provide documentation address for frontend

--- src/config/Frontend.ts
+++ src/config/Frontend.ts
@@ -3,5 +3,6 @@ export default {
   AUTHENTIK_URL: process.env.AUTHENTIK_URL || 'http://authentik:9000',
   AUTHENTIK_NAME: process.env.AUTHENTIK_NAME || 'Authentik',
   MAIL_FEEDBACK_ADDRESS: process.env.MAIL_FEEDBACK_ADDRESS || 'feedback@jhaas.local',
-  TOS_ADDRESS: process.env.TOS_ADDRESS || '/api/tos/latest'
+  TOS_ADDRESS: process.env.TOS_ADDRESS || '/api/tos/latest',
+  DOCS_ADDRESS: process.env.DOCS_ADDRESS || 'http://localhost:8081'
 };
