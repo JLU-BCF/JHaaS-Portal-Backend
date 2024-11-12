678ec76f5c9c93eed151773970eaa69a4ed09bc1
Nils Mittler <nils.mittler@computational.bio.uni-giessen.de>
2024-11-12 13:45:12 +0100

    Enable authenticated smtp mail send

--- src/config/Mail.ts
+++ src/config/Mail.ts
@@ -4,6 +4,9 @@ export const MAIL_HOST: string = process.env.MAIL_HOST || 'mailhog';
 export const MAIL_PORT: number = Number(process.env.MAIL_PORT) || 2525;
 export const MAIL_SECURE: boolean = ['true', true, 1].includes(process.env.MAIL_SECURE);
 
+export const MAIL_USER: string = process.env.MAIL_USER;
+export const MAIL_PASS: string = process.env.MAIL_PASS;
+
 export const MAIL_TEMPLATE_DIR: string =
   process.env.MAIL_TEMPLATE_DIR || `${__dirname}/../../templates/mail`;
 
@@ -13,10 +16,9 @@ export const MAIL_COPY_ADDRESSES: string[] = process.env.MAIL_COPY_ADDRESSES
   ? JSON.parse(process.env.MAIL_COPY_ADDRESSES)
   : ['admins@jhaas.local'];
 
-// TODO: options for authenticated mail delivery
-
 export const mailTransporter = nodemailer.createTransport({
   host: MAIL_HOST,
   port: MAIL_PORT,
-  secure: MAIL_SECURE
+  secure: MAIL_SECURE,
+  auth: MAIL_USER && MAIL_PASS ? { user: MAIL_USER, pass: MAIL_PASS } : undefined
 });
